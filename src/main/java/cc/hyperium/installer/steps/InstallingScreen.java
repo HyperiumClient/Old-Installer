f4fb6b7ff6f7b27f1c91e612d7aa438587621354
VRCube <vrcproxz@gmail.com>
2018-07-09 13:17:58 +0200

    nice blob loading gif :D

--- src/main/java/cc/hyperium/installer/steps/InstallingScreen.java
+++ src/main/java/cc/hyperium/installer/steps/InstallingScreen.java
@@ -31,7 +31,13 @@ public class InstallingScreen extends InstallerStep {
         title.setForeground(Color.WHITE);
         title.setBounds(0, panel.getHeight() / 5 * 4, panel.getWidth(), 40);
 
+        int w = Math.min(panel.getHeight() / 3, 366);
+        JLabel blob = new JLabel(new ImageIcon(new ImageIcon(getClass().getResource("/icons/loading.gif")).getImage().getScaledInstance(w, w, Image.SCALE_DEFAULT)));
+        blob.setBounds(panel.getWidth() / 2 - w / 2, panel.getHeight() / 2 - w / 2, w, w);
+        blob.setSize(w, w);
+
         panel.add(title);
+        panel.add(blob);
 
         Multithreading.runAsync(() -> {
             try {
