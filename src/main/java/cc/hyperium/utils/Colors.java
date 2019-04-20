3f37b5044c294647c7ceb8e4d56bac47cee593b2
Reece Dunham <me@rdil.rocks>
2019-04-20 19:55:28 -0400

    Cleanup

--- src/main/java/cc/hyperium/utils/Colors.java
+++ src/main/java/cc/hyperium/utils/Colors.java
@@ -2,9 +2,6 @@ package cc.hyperium.utils;
 
 import java.awt.Color;
 
-/*
- * Created by Cubxity on 05/07/2018
- */
 public final class Colors {
     public static final Color DARK = new Color(30, 30, 30);
 }
