f7e74ba28d265ad0a833c1b91eaa3660fa9dbe90
Reece Dunham <me@rdil.rocks>
2019-04-20 20:03:25 -0400

    Cleanup

--- src/main/java/cc/hyperium/installer/api/callbacks/Phrase.java
+++ src/main/java/cc/hyperium/installer/api/callbacks/Phrase.java
@@ -1,71 +1,16 @@
 package cc.hyperium.installer.api.callbacks;
 
-/*
- * Created by Cubxity on 08/07/2018
- */
 public enum Phrase {
-
-    /**
-     * Pre checks before installation process
-     */
     PRE,
-
-    /**
-     * When it's copying '1.8.9' version
-     */
     COPY_VERSION,
-
-    /**
-     * Copies the jar installer is running from (LOCAL)
-     */
     COPY_LOCAL,
-
-    /**
-     * When it's downloading client
-     */
     DOWNLOAD_CLIENT,
-
-    /**
-     * Client integrity verification
-     */
     VERIFY_CLIENT,
-
-    /**
-     * When it's downloading components such as Optifine and adddons
-     */
     DOWNLOAD_COMPONENTS,
-
-    /**
-     * Addon integrity verification
-     */
     VERIFY_COMPONENTS,
-
-    /**
-     * When it's patching optifine, only when user has selected it
-     */
     PATCH_OPTIFINE,
-
-    /**
-     * @since 1.3
-     *
-     * When it's downloading launchwrapper
-     */
     DOWNLOAD_LAUNCHER,
-
-    /**
-     * @since 1.3
-     *
-     * When it's patching launchwrapper
-     */
     PATCH_LAUNCHER,
-
-    /**
-     * When it's creating launcher profile
-     */
     CREATE_PROFILE,
-
-    /**
-     * When installation has finished
-     */
     DONE
 }
